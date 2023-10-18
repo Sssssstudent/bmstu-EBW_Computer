f605a052c2f09699cde2fa5c951f19f014fd39f3
Alex Vol <alexvol@mail.ru>
2023-10-18 22:54:32 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def print_hellow():
+    return render_template("index.html")
+
+
+@app.route('/text/')
+def print_text():
+    return '<h1>Some text!</h1>'
+
+
+app.run()
