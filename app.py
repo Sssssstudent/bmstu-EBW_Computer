dc1e90a15e7573981425d1c1d3eafd75e3d0ea20
Alex Vol <alexvol@mail.ru>
2023-10-20 14:41:58 +0300

    Global changes

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def print_hellow():
-    return render_template("index.html")
+model_loaded = joblib.load('ml_models/models/ebw.pkl')
+scaler_loaded = joblib.load('ml_models/scalers/MinMaxScaler.pkl')
 
 
-@app.route('/text/')
-def print_text():
-    return '<h1>Some text!</h1>'
+@app.route('/', methods=["get", "post"])
+def predict():
+    data = None
+    if request.method == "POST":
+        iw = float(request.form.get("iw"))
+        i_f = float(request.form.get("if"))
+        vw = float(request.form.get("vw"))
+        fp = float(request.form.get("fp"))
+
+        test_sample = scaler_loaded.transform([[iw, i_f, vw, fp]])
+        print(test_sample)
+
+        prediction = model_loaded.predict(test_sample)
+
+        weld_depth = prediction[0][0]
+        weld_width = prediction[0][1]
+        data = {
+            'iw': iw,
+            'if': i_f,
+            'vw': vw,
+            'fp': fp,
+            'weld_depth': weld_depth,
+            'weld_width': weld_width,
+        }
+
+    return render_template(
+        "index.html",
+        data=data
+    )
 
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=False)
